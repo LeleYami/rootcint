4d1d1caa7e33d67327d61e2676427989c379fb89
LeleYami <524379973@qq.com>
2016-11-15 15:15:52 +0800

    Create LinkDef.h

---
+++ LinkDef.h
@@ -0,0 +1,10 @@
+#ifdef_ _CINT_ _
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class class Sclass+;
+
+#endif
